ae424430af6c956796236ec6c3673e005a737c1c
Nick T <nicholastroiano123@gmail.com>
2023-12-07 13:57:47 -0500

    oh yeaaaa

---
+++ main.py
@@ -0,0 +1,117 @@
+data = {
+    {
+        'name': 'early-lunch',
+        'schedule': {
+            {
+                'id': 'advisory',
+                'start': {'h': 8, 'm': 5},
+                'end': {'h': 8, 'm': 5},
+            },
+            {
+                'id': 'block-1',
+                'start': {'h': 8, 'm': 10},
+                'end': {'h': 9, 'm': 10},
+            },
+            {
+                'id': 'block-2',
+                'start': {'h': 9, 'm': 15},
+                'end': {'h': 10, 'm': 15},
+            },
+            {
+                'id': 'block-3',
+                'start': {'h': 10, 'm': 20},
+                'end': {'h': 11, 'm': 20},
+            },
+            {
+                'id': 'flex',
+                'start': {'h': 11, 'm': 25},
+                'end': {'h': 11, 'm': 55},
+            },
+            {
+                'id': 'lunch-early',
+                'start': {'h': 12, 'm': 0},
+                'end': {'h': 12, 'm': 25},
+            },
+            {
+                'id': 'block-4-late',
+                'start': {'h': 12, 'm': 30},
+                'end': {'h': 13, 'm': 30},
+            },
+            {
+                'id': 'block-5',
+                'start': {'h': 13, 'm': 35},
+                'end': {'h': 14, 'm': 35},
+            },
+            {
+                'id': 'collab',
+                'start': {'h': 14, 'm': 35},
+                'end': {'h': 15, 'm': 15},
+            },
+        }
+    },
+    {
+        'name': 'late-lunch',
+        'schedule': {
+            {
+                'id': 'advisory',
+                'start': {'h': 8, 'm': 5},
+                'end': {'h': 8, 'm': 5},
+            },
+            {
+                'id': 'block-1',
+                'start': {'h': 8, 'm': 10},
+                'end': {'h': 9, 'm': 10},
+            },
+            {
+                'id': 'block-2',
+                'start': {'h': 9, 'm': 15},
+                'end': {'h': 10, 'm': 15},
+            },
+            {
+                'id': 'block-3',
+                'start': {'h': 10, 'm': 20},
+                'end': {'h': 11, 'm': 20},
+            },
+            {
+                'id': 'flex',
+                'start': {'h': 11, 'm': 20},
+                'end': {'h': 12, 'm': 00},
+            },
+            {
+                'id': 'block-4-early',
+                'start': {'h': 12, 'm': 00},
+                'end': {'h': 13, 'm': 00},
+            },
+            {
+                'id': 'lunch-late',
+                'start': {'h': 13, 'm': 30},
+                'end': {'h': 13, 'm': 35},
+            },
+            {
+                'id': 'block-5',
+                'start': {'h': 13, 'm': 35},
+                'end': {'h': 14, 'm': 35},
+            },
+            {
+                'id': 'collab',
+                'start': {'h': 14, 'm': 35},
+                'end': {'h': 15, 'm': 15},
+            },
+        }
+    }
+}
+
+classData = {
+    'logic': +1,
+    'data': {
+        'period-1': {'name': 'Math', 'args': ['early']},
+        'period-2': {'name': 'Health', 'args': ['early']},
+        'period-3': {'name': 'English', 'args': ['early']},
+        'period-4': {'name': 'Free', 'args': ['early']},
+        'period-5': {'name': 'Spanish', 'args': ['late']},
+        'period-6': {'name': 'History', 'args': ['early']},
+        'period-7': {'name': 'Bio', 'args': ['late']},
+        'period-8': {'name': 'Jazz Band', 'args': ['late']}
+    }
+}
+
