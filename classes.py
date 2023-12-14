f5a054dfa038bc7502f1ebd59abceb7a04abe8c4
JohannesWM <97265854+JohannesWM@users.noreply.github.com>
2023-12-14 09:38:22 -0500

    Updates to classes.py

--- classes.py
+++ classes.py
@@ -7,11 +7,28 @@ class Period:
         self.teacher = teacher
         self.period_num = period_num
 
+        self.check_attributes()
+
     def __repr__(self):
         return "Period()"
 
     def __str__(self):
         return f"Period() obj: {self.title}"
+
+    def check_attributes(self):
+        check = lambda name, d_type: True if type(name) is not type(d_type) else False
+        if check(self.title, ""):
+            raise TypeError("attribute \'title\' must be string")
+        if check(self.color, ""):
+            raise TypeError("attribute \'color\' must be string")
+        if check(self.start_time, ""):
+            raise TypeError("attribute \'start_time\' must be string")
+        if check(self.end_time, ""):
+            raise TypeError("attribute \'end_time\' must be string")
+        if check(self.teacher, ""):
+            raise TypeError("attribute \'teacher\' must be string")
+        if check(self.period_num, int):
+            raise TypeError("attribute \'period_num\' must be int")
 
 
 class SchoolDay:
@@ -39,6 +56,9 @@ class SchoolDay:
             raise TypeError("attribute \'color\' must be string")
         if check(self.periods, []):
             raise TypeError("attribute \'periods\' must be list")
+        if self.periods.__len__() > 0:
+            if check(self.periods, Period):
+                raise TypeError("attributes within \'periods\' are not Period objects")
 
 
 day_A = SchoolDay("Day A", "Red")
